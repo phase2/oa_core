05c100b6010f6dbdaa509ee90572e6baf6b516fe
mpotter <mpotter@phase2technology.com>
2013-08-20 15:42:26 +0000

    Fix body padding when navbar not present

--- modules/oa_dashboard/js/oa-dashboard.js
+++ modules/oa_dashboard/js/oa-dashboard.js
@@ -10,14 +10,24 @@
       var $oa_navbar = $('#oa-navbar');
       var $navbar = $('#navbar-bar');
 
-      if ($oa_navbar.length && $navbar.length) {
+      var $height = 0;
+      if ($navbar.length) {
         // if using OA navbar, hide the navbar module's nav bar and remove its offset
         $navbar.hide();
         $navbar.removeAttr('data-offset-top');
+      }
+      if ($oa_navbar.length) {
         if (settings.oa_toolbar_sticky == 1 && $oa_navbar.css('position') !== 'static') {
           $oa_navbar.attr('data-offset-top', '');
+          $height = $oa_navbar.height();
         }
+      }
+      if (typeof(Drupal.displace) != "undefined") {
         Drupal.displace();  // recalculate offsets
+      }
+      else if ($height > 0) {
+        // navbar not being used, so just offset the body padding
+        $('body').css('padding-top', $height);
       }
     }
   }
