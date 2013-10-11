4a540618c7bac712418eca0be88a581648d7fbb3
mpotter <mpotter@phase2technology.com>
2013-10-11 14:34:55 +0000

    Fixed transition problems in banners

--- modules/oa_appearance/oa_appearance.js
+++ modules/oa_appearance/oa_appearance.js
@@ -5,58 +5,70 @@
 
 (function ($) {
 
+  var $total_height;
+
   Drupal.behaviors.oaAppearance = {
     attach: function(context, settings) {
-      var $oa_navbar = $('#oa-navbar');
-      var $banner = $('.oa-banner');
+      var $oa_navbar = $('#oa-navbar', context);
+      var $banner = $('.oa-banner', context);
       if ($oa_navbar.length && $banner.length) {
         var $height = $oa_navbar.height();
         var $width = $oa_navbar.width();
+        $total_height = $height;
         $banner.each( function(index) {
-          // set the image sizes before image gets loaded
-          var $img_width = $(this).attr('data-width');
-          var $img_height = $(this).attr('data-height');
-          if ($img_width > 0) {
-            var $new_height = $img_height * $width / $img_width;
-            var $new_width = $new_height * $img_width / $img_height;
-            $(this).css('height', $new_height);
-            $(this).addClass('oa-banner-hidden');
-            var $image = $('.oa-banner-overlay-img', this);
-            $image.css('width', '100%');
-          }
-          else {
-            $(this).css('height', 'auto');
-            $(this).addClass('oa-banner-appeared');
+          if (!$(this).hasClass('oa-banner-appeared') &&
+              !$(this).hasClass('oa-banner-hidden')) {
+            // set the image sizes before image gets loaded
+            var $img_width = $(this).attr('data-width');
+            var $img_height = $(this).attr('data-height');
+            var $new_height = $img_height;
+            if ($img_width > 0) {
+              $new_height = $img_height * $width / $img_width;
+            }
+            if ($(this).parents('#oa-navbar').length) {
+              // this image is within the navbar
+              $total_height -= $(this).height();
+              $total_height += $new_height;
+            }
+            if ($img_width > 0) {
+              // stretched banner image
+              var $new_width = $new_height * $img_width / $img_height;
+              $(this).css('height', $new_height);
+              $(this).addClass('oa-banner-hidden');
+              var $image = $('.oa-banner-overlay-img', this);
+              if ($image.length) {
+                $image.css('width', '100%');
+              }
+            }
+            else {
+              // non-stretching banner image
+              $(this).css('height', $new_height);
+              $(this).addClass('oa-banner-appeared');
+            }
           }
         });
+        if ($total_height > 0) {
+          $oa_navbar.height($total_height);
+          $('body').css('padding-top', $total_height);
+        }
+        if (typeof(Drupal.displace) != "undefined") {
+          Drupal.displace();  // recalculate offsets
+        }
       }
     }
   }
 
   $(window).load(function() {
+    var $oa_navbar = $('#oa-navbar');
     var $image = $('.oa-banner-overlay-img');
     var $banner = $('.oa-banner');
-    if ($image.length) {
-
-//      $image.css( 'height', 'auto'); // let the banner resize from now on
+    if ($banner.length) {
+      $banner.css( 'height', 'auto'); // let the banner resize from now on
       $banner.addClass('oa-banner-appeared');
       $banner.removeClass('oa-banner-hidden');
     }
-    // now that images are loaded, recalculate body offset for images in the
-    // static toolbar
-    var $oa_navbar = $('#oa-navbar');
-    if ($oa_navbar.length) {
-      if ($oa_navbar.css('position') !== 'static') {
-        $height = $oa_navbar.height();
-      }
-    }
-    if (typeof(Drupal.displace) != "undefined") {
-      Drupal.displace();  // recalculate offsets
-    }
-    else if ($height > 0) {
-      // navbar not being used, so just offset the body padding
-      $('body').css('padding-top', $height);
-    }
+    // this is needed to allow bootstrap tour to display correctly
+    $(window).trigger('resize');
   });
 
 }(jQuery));
