f1035cd85a6744ba93d0581fe80ce8bab1047e31
mpotter <mpotter@phase2technology.com>
2013-10-11 17:57:51 +0000

    Add oa_navigation widget for main menu nav

---
+++ modules/oa_dashboard/oa-navigation.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Template for OA Navigation.
+ */
+?>
+<div class="oa-navigation clearfix">
+  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+  <a class="btn btn-navbar btn-navbar-menu">
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+  </a>
+
+  <div class="nav-collapse nav-menu-collapse">
+    <?php if ($menu): ?>
+      <nav class="main-menu pull-left" role="navigation">
+        <?php print render($menu); ?>
+      </nav>
+    <?php endif; ?>
+  </div>
+</div>
