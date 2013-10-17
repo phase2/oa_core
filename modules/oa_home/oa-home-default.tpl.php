0a0dfb2584a3e00b4c51ee61d31346f377be0408
mpotter <mpotter@phase2technology.com>
2013-10-17 18:29:17 +0000

    More work on default home page, added user login

--- modules/oa_home/oa-home-default.tpl.php
+++ modules/oa_home/oa-home-default.tpl.php
@@ -9,12 +9,21 @@
     <div class="hero-left-graphic"></div><!--/hero-right-graphic-->
     <div class="hero-right-graphic"></div><!--/hero-right-graphic-->
     <h1>Powerful, Flexible, Open</h1>
-    <h2>Welcome to Open Atrium 2!</h2>
+    <h2>Welcome <?php if (!empty($username)) {print '<span>' . $username . '</span>'; } ?> to Open Atrium 2!</h2>
     <h3>What's next?  Dive in or check out some resources.</h3>
   </div><!--/hero-->
 
   <div class="modules">
 
+  <?php if (!empty($login)): ?>
+    <div class="entry">
+      <div class="module-header"><h4>just getting started?</h4></div><!--/module-header-->
+      <div class="module-content">
+        <a href="http://docs.openatrium.com/" class="icon icon-documentation"></a>
+        <p>Read <a href="http://docs.openatrium.com/">documentation</a> to help you get going with Open Atrium, or Visit the open source <a href="https://drupal.org/project/openatrium">Open Atrium project</a> at <a href="https://drupal.org">drupal.org.</a></p>
+      </div><!--/module content-->
+    </div><!--/entry-->
+  <?php else: ?>
     <div class="entry">
       <div class="module-header"><h4>got issues?</h4></div><!--/module-header-->
       <div class="module-content">
@@ -22,7 +31,13 @@
         <p>Visit the open source <a href="https://drupal.org/project/openatrium">Open Atrium project</a> at <a href="https://drupal.org">drupal.org.</a></p>
       </div><!--/module content-->
     </div><!--/entry-->
+  <?php endif; ?>
 
+  <?php if (!empty($login)): ?>
+    <div class="user-login">
+      <?php print render($login); ?>
+    </div>
+  <?php else: ?>
     <div class="entry">
       <div class="module-header"><h4>just getting started?</h4></div><!--/module-header-->
       <div class="module-content">
@@ -30,6 +45,7 @@
         <p>Read <a href="http://docs.openatrium.com/">documentation</a> to help you get going with Open Atrium.</p>
       </div><!--/module content-->
     </div><!--/entry-->
+  <?php endif; ?>
 
     <div class="entry entry-last">
       <div class="module-header"><h4>need the pros?</h4></div><!--/module-header-->
@@ -42,7 +58,8 @@
   </div><!--/modules-->
 
   <div class="subfooter">
-    <div class="subfooter-graphic"></div><!--/subfooter graphic-->
+    <div class="subfooter-graphic">
+    </div><!--/subfooter graphic-->
   </div><!--/subfooter-->
 
 </div><!--/big wrapper-->
