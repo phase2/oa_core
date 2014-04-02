fd83b669c52f1bfb053aab1f7e33abb0bfc4fe1f
blatin <blatin@wearelanding.com>
2014-04-02 11:39:29 -0700

    HITRCMIGRATION-34: User Profile- User Notification Settings

--- modules/oa_messages/js/oa-messages-settings.js
+++ modules/oa_messages/js/oa-messages-settings.js
@@ -6,57 +6,11 @@
 (function ($) {
 
   Drupal.behaviors.messagesSettings = {
-    attach: function(context, settings) {
-      var $form = $('#edit-oa-messages');
-      var $tabs = $('#messages-tabs');
-      
-      // Add tab functionality.
-      $tabs.find('a').click(function(e) {
-        $(this).tab('show');
-      });
-      $tabs.find('a:first').tab('show');
-      
-      // Add check / uncheck all buttons to spaces / messages.
-      var $spaceLabels = $form.find('.space-label');
-      $spaceLabels.each(function() {
-        var $checkboxes = $(this).parent().find('input[type="checkbox"]');
-        addCheckUncheckAllButtons($(this), $checkboxes);
-      });
-      
-      var $messageLabels = $form.find('.checkbox-table th:not(:first-child)');
-      var index = 2;
-      var paneId = false;
-      $messageLabels.each(function() {
-        var $pane = $(this).parents('.tab-pane');
-        
-        // Reset index on new tab pane.
-        if (paneId && $pane.attr('id') !== paneId) {
-          index = 2;
-        }
-        paneId = $pane.attr('id');
-        
-        var $checkboxes = $pane.find('.checkbox-table td:nth-child(' + index + ') input[type="checkbox"]');
-        addCheckUncheckAllButtons($(this), $checkboxes);
-        index++;
+    attach: function (context, settings) {
+      $('.select-all').click(function() {
+        $('.apply-to').prop('checked', this.checked);
       });
     }
-  }
-  
-  // Add check / uncheck all buttons to an element.
-  function addCheckUncheckAllButtons(element, checkboxes) {
-    var buttons  = '<div class="check-buttons">';
-        buttons += '  <a class="btn check-all"><i class="icon-ok"></i></a>';
-        buttons += '  <a class="btn uncheck-all"><i class="icon-remove"></i></a>';
-        buttons += '</div>';
-    $(element).append(buttons);
-    
-    $(element).find('.check-all').click(function() {
-      console.log($(checkboxes));
-      $(checkboxes).attr('checked', true);
-    });
-    $(element).find('.uncheck-all').click(function() {
-      $(checkboxes).attr('checked', false);
-    });
-  }
+  };
 
 })(jQuery);
