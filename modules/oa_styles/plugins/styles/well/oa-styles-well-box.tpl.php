6dff34d58c4a0607ac10025aa53e3a320976790b
Fox <hefoxed@gmail.com>
2014-09-29 10:52:00 -0700

    Issue #2330057: remove oa_styles as it's its own app

--- modules/oa_styles/plugins/styles/well/oa-styles-well-box.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * This template handles the layout of oa_well.
- */
-?>
-<div class="well style-region <?php if (isset($settings['well_modifier'])): print $settings['well_modifier']; endif; ?>">
-  <?php print $content; ?>
-</div>
